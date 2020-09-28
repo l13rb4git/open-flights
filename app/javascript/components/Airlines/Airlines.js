7d5bd75fe2e980a3683ce6426fb3f89a35c432be
macunaima <gcgamer46@gmail.com>
2020-09-28 13:54:25 -0300

    Show data getted from API on the page

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, Fragment } from 'react'
 import axios from 'axios'
 
 
@@ -8,13 +8,26 @@ const Airlines = () => {
 
     useEffect(() => {
         axios.get('api/v1/airlines.json')
-        .then( resp => console.log(resp) )
+        .then( resp => {
+            setAirlines(resp.data.data)
+        })
         .catch( resp => console.log(resp) )
     }, [airlines.length])
 
+    const list = airlines.map( item => {
+        return (<li key={item.attributes.name}>{item.attributes.name}</li>)
+    })
 
     return (
-        <div>Airlines#Index</div>
+        <div className="home">
+            <div className="header">
+                <h1>OpenFlights</h1>
+                <div className="subheader">Honest, unbiased airlines review.</div>
+            </div>
+            <div className="grid">
+                <ul>{list}</ul>
+            </div>
+        </div>
     )
 }
 
