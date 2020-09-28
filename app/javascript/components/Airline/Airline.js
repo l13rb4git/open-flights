dadb05761af507695ba14a589962b8748ba10b31
macunaima <gcgamer46@gmail.com>
2020-09-28 13:27:27 -0300

    Added React to front-end

---
+++ app/javascript/components/Airline/Airline.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+
+const Airline = () => {
+    return <div>Airlines#Show </div>
+}
+
+export default Airline
---
+++ app/javascript/components/Airlines/Airlines.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+
+const Airlines = () => {
+    const [airlines, setAirlines] = useState([]);
+
+
+    useEffect(() => {
+        axios.get('api/v1/airlines.json')
+        .then( resp => console.log(resp) )
+        .catch( resp => console.log(resp) )
+    }, [airlines.length])
+
+
+    return (
+        <div>Airlines#Index</div>
+    )
+}
+
+export default Airlines
