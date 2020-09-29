6f8b700ce0e0b7bebf292de7f158d9ee85be2567
macunaima <gcgamer46@gmail.com>
2020-09-29 09:25:15 -0300

    Getting the correct slug

--- app/javascript/components/Airline/Airline.js
+++ app/javascript/components/Airline/Airline.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 
-const Airline = () => {
+const Airline = (props) => {
+  const [airline, setAirline] = useState({})
+  const [review, setReview] = useState({})
+
+
+  useEffect(() => {
+    const slug = props.match.params.slug
+    const url = `api/v1/airlines/${slug}`
+  }, [])
+
   return <div>Airlines#Show </div>
 }
 
