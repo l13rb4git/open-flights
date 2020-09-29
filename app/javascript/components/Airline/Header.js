188a2df3224a50be538c872a2acbfa1ecf4370f5
macunaima <gcgamer46@gmail.com>
2020-09-29 09:57:44 -0300

    Header component created

---
+++ app/javascript/components/Airline/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+const Header = (props) => {
+  return (
+    <div class="wrapper">
+      <h1>
+        <img src="" alt="">
+        Airline Name
+      </h1>
+      <div>
+        <div class="totalReviews"></div>
+        <div class="starRating"></div>
+        <div class="totalOutOf">3 out 5</div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
