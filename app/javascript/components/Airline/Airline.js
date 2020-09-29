973af3301087fadd2a3ac4afcf5e5201304a4926
macunaima <gcgamer46@gmail.com>
2020-09-29 09:38:39 -0300

    getting data from API

--- app/javascript/components/Airline/Airline.js
+++ app/javascript/components/Airline/Airline.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 
 const Airline = (props) => {
@@ -8,7 +9,11 @@ const Airline = (props) => {
 
   useEffect(() => {
     const slug = props.match.params.slug
-    const url = `api/v1/airlines/${slug}`
+    const url = `/api/v1/airlines/${slug}`
+
+    axios.get(url)
+      .then( resp => setAirline(resp.data) )
+      .catch( resp => console.log(resp) )
   }, [])
 
   return <div>Airlines#Show </div>
