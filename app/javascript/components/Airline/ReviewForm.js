bbeb560f118a6a411d12ae3e6581d57e4c928963
macunaima <gcgamer46@gmail.com>
2020-09-29 11:53:19 -0300

    FIX: A component is changing an uncontrolled input of type text to be controlled.

--- app/javascript/components/Airline/ReviewForm.js
+++ app/javascript/components/Airline/ReviewForm.js
@@ -7,10 +7,10 @@ const ReviewForm = (props) => {
       <from onSubmit={props.handleSubmit}>
         <div>Have an experience with {props.attributes.name}? Share a review!</div>
         <div className="field">
-          <input onChange={props.handleChange} value={props.review.title} type="text" name="title" placeholder="Review Title"/>
+          <input onChange={props.handleChange} value={props.review.title || ''} type="text" name="title" placeholder="Review Title"/>
         </div>
         <div className="field">
-          <input onChange={props.handleChange} value={props.review.description} type="text" name="description" placeholder="Review Description"/>
+          <input onChange={props.handleChange} value={props.review.description || ''} type="text" name="description" placeholder="Review Description"/>
         </div>
         <div className="field">
           <div className="ratingContainer">
