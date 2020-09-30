4ae0ed3a5c5647d21c6510f866b9205c35a030fe
macunaima <gcgamer46@gmail.com>
2020-09-30 12:22:14 -0300

    Rating component created

---
+++ app/javascript/components/Rating/Rating.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+const StarWrapper = styled.span`
+`
+
+const Stars = styled.span`
+`
+
+
+const Rating = (props) => {
+  return (
+    <StarWrapper>
+      <Stars></Stars>
+    </StarWrapper>
+  )
+}
+
+
+export default Rating
