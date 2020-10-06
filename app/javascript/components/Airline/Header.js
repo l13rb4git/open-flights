3ddfbeef000a7d22afe38862cf189eb5a116cce7
macunaima <gcgamer46@gmail.com>
2020-10-06 09:36:07 -0300

    Spacing the image to the airline name

--- app/javascript/components/Airline/Header.js
+++ app/javascript/components/Airline/Header.js
@@ -13,6 +13,7 @@ const Wrapper = styled.div`
         border-radius: 100%;
         border: 1px solid rgba(0, 0, 0, 0.1);
         margin-bottom: -8px;
+        margin-right: 20px;
     }
 
     h1 {
